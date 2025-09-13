d373e468377810da57642735d14789293cde8afc
VijayChCode <chalendravijay09@gmail.com>
2025-09-13 14:39:38 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -614,7 +614,7 @@ export default function SignIn({ bootstrapped, sessionChecked }) {
                                 <div className="w-full border-t border-gray-300"></div>
                             </div>
                             <div className="relative flex justify-center text-sm">
-                                <span className="px-2 bg-white text-gray-500">Or continue with</span>
+                                <span className="px-2 bg-white text-gray-500">OR</span>
                             </div>
                         </div>
                         
