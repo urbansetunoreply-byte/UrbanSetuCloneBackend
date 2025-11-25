5efbd598ba7180ff574f36b67b752d0ea32dc2af
VijayChCode <chalendravijay09@gmail.com>
2025-11-25 13:53:22 +0530

    Remove visibility parameter from fetch requests
    Removed visibility parameter from fetch requests in fetchListings and showMoreListingClick functions.

--- web/src/pages/AdminExplore.jsx
+++ web/src/pages/AdminExplore.jsx
@@ -79,9 +79,7 @@ export default function AdminExplore() {
     const fetchListings = async () => {
       setLoading(true);
       try {
-        const fetchParams = new URLSearchParams(urlParams);
-        fetchParams.set('visibility', 'public');
-        const res = await fetch(`${API_BASE_URL}/api/listing/get?${fetchParams.toString()}`);
+        const res = await fetch(`${API_BASE_URL}/api/listing/get?${urlParams.toString()}`);
         const data = await res.json();
         setListings(data);
         setShowMoreListing(data.length > 8);
@@ -676,7 +674,6 @@ export default function AdminExplore() {
   const showMoreListingClick = async () => {
     const urlParams = new URLSearchParams(location.search);
     urlParams.set("startIndex", listings.length);
-    urlParams.set("visibility", "public");
     const res = await fetch(`${API_BASE_URL}/api/listing/get?${urlParams.toString()}`);
     const data = await res.json();
     setListings((prev) => [...prev, ...data]);
