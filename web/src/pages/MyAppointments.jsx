8478e03de8db3a9d083b0a51ea18efb68f2b5cc5
Vijay <chalendravijay09@gmail.com>
2026-01-03 20:40:38 +0530

    Image  preview fix34

--- web/src/pages/MyAppointments.jsx
+++ web/src/pages/MyAppointments.jsx
@@ -18,7 +18,6 @@ import CallHistoryModal from '../components/CallHistoryModal';
 import ChatSettingsModal from '../components/ChatSettingsModal';
 import { useChatSettings } from '../hooks/useChatSettings';
 import ImagePreview from '../components/ImagePreview';
-import VideoPreview from '../components/VideoPreview';
 import VideoMessageBubble from '../components/VideoMessageBubble';
 import LinkPreview from '../components/LinkPreview';
 import UserAvatar from '../components/UserAvatar';
