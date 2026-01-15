f3b20bb1e2476bd65399a110eb4a15ee625fea25
Vijay <chalendravijay09@gmail.com>
2026-01-16 00:40:20 +0530

    style(ui): Remove hover effect from EncryptedText username on profile page

--- web/src/components/ui/EncryptedText.jsx
+++ web/src/components/ui/EncryptedText.jsx
@@ -40,35 +40,9 @@ const EncryptedText = ({ text, className = "", interval = 50 }) => {
         return () => clearInterval(timer);
     }, [text, interval]);
 
-    // Optional: Re-run effect on hover for extra interactivity
-    const handleMouseEnter = () => {
-        setIsHovering(true);
-        let iteration = 0;
-        let timer = setInterval(() => {
-            setDisplayText(prev =>
-                text
-                    .split("")
-                    .map((char, index) => {
-                        if (index < iteration) {
-                            return text[index];
-                        }
-                        return chars[Math.floor(Math.random() * chars.length)];
-                    })
-                    .join("")
-            );
-
-            if (iteration >= text.length) {
-                clearInterval(timer);
-            }
-
-            iteration += 1 / 3;
-        }, 30);
-    };
-
     return (
         <span
             className={`font-mono ${className}`}
-            onMouseEnter={handleMouseEnter}
             style={{ display: 'inline-block' }} // ensures proper spacing
         >
             {displayText || text}
