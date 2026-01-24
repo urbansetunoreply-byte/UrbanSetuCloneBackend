749b464831330d07186ce0b426553d3b3fba8cad
Vijay <chalendravijay09@gmail.com>
2026-01-24 12:32:16 +0530

    Theme fix

--- web/src/hooks/useSeasonalTheme.js
+++ web/src/hooks/useSeasonalTheme.js
@@ -1,4 +1,30 @@
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
+
+const IndiaFlagIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600" className="inline-block align-middle" style={{ width: '1.2em', height: '0.8em' }}>
+        <path fill="#FF9933" d="M0 0h900v200H0z" />
+        <path fill="#FFF" d="M0 200h900v200H0z" />
+        <path fill="#138808" d="M0 400h900v200H0z" />
+        <g transform="translate(450 300)">
+            <circle r="92.5" fill="none" stroke="#000080" strokeWidth="15" />
+            <circle r="16" fill="#000080" />
+            <g id="d">
+                <g id="c">
+                    <g id="b">
+                        <g id="a" fill="#000080">
+                            <path d="M0-92.5L5.5-8.5l-11 0z" />
+                        </g>
+                        <use href="#a" transform="rotate(15)" />
+                    </g>
+                    <use href="#b" transform="rotate(30)" />
+                </g>
+                <use href="#c" transform="rotate(60)" />
+            </g>
+            <use href="#d" transform="rotate(120)" />
+            <use href="#d" transform="rotate(240)" />
+        </g>
+    </svg>
+);
 
 export const useSeasonalTheme = () => {
     const theme = useMemo(() => {
@@ -60,7 +86,7 @@ export const useSeasonalTheme = () => {
                 id: 'armyday',
                 name: 'Indian Army Day',
                 icon: '🫡',
-                secondaryIcon: '🇮🇳',
+                secondaryIcon: IndiaFlagIcon,
                 greeting: 'Saluting our Heroes!',
                 effect: 'tricolor',
                 textGradient: 'bg-gradient-to-r from-orange-600 via-white to-green-700',
@@ -73,7 +99,7 @@ export const useSeasonalTheme = () => {
             return {
                 id: 'republic',
                 name: 'Republic Day',
-                icon: '🇮🇳',
+                icon: IndiaFlagIcon,
                 secondaryIcon: '🫡',
                 greeting: 'Happy Republic Day!',
                 effect: 'tricolor',
@@ -213,7 +239,7 @@ export const useSeasonalTheme = () => {
             return {
                 id: 'independence',
                 name: 'Independence Day',
-                icon: '🇮🇳',
+                icon: IndiaFlagIcon,
                 secondaryIcon: '🎆',
                 greeting: 'Happy Independence Day!',
                 effect: 'tricolor',
